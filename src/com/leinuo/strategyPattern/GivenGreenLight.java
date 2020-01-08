836a4f6423fc3863eb8a3299fdfd779758b48621
liujb <1321404703@qq.com>
2020-01-08 17:26:37 +0800

    策略模式

---
+++ src/com/leinuo/strategyPattern/GivenGreenLight.java
@@ -0,0 +1,12 @@
+package com.leinuo.strategyPattern;
+
+/**
+ * Create by leinuo on 20-1-8 下午5:17
+ * <p>
+ * qq:1321404703 https://github.com/leinuo2016
+ */
+public class GivenGreenLight implements IStrategy {
+    public void operate() {
+        System.out.println("求吴国太开个绿灯,放行!");
+    }
+}
---
+++ src/com/leinuo/strategyPattern/ZhaoYun.java
@@ -0,0 +1,21 @@
+package com.leinuo.strategyPattern;
+
+/**
+ * Create by leinuo on 20-1-8 下午5:22
+ * <p>
+ * qq:1321404703 https://github.com/leinuo2016
+ */
+public class ZhaoYun {
+
+    public static void main(String[] args) {
+        Context context;
+        context = new Context(new BackDoor());
+        context.operate();
+
+        context = new Context(new GivenGreenLight());
+        context.operate();
+
+        context = new Context(new BlockEnemy());
+        context.operate();
+    }
+}
