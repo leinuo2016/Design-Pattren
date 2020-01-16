798b716c68b939f6939e1d69873638f5caa0fb13
liujb <1321404703@qq.com>
2020-01-16 17:20:49 +0800

    建造者模式

---
+++ src/com/leinuo/builderPattern/BMWBuilder.java
@@ -0,0 +1,22 @@
+package com.leinuo.builderPattern;
+
+import java.util.ArrayList;
+
+/**
+ * Create by leinuo on 20-1-16 下午4:57
+ * <p>
+ * qq:1321404703 https://github.com/leinuo2016
+ */
+public class BMWBuilder extends CarBuilder {
+
+    private BMWModel bmw = new BMWModel();
+    @Override
+    public void setSequence(ArrayList<String> sequence) {
+        this.bmw.setSequence(sequence);
+    }
+
+    @Override
+    public CarModel getCarModel() {
+        return bmw;
+    }
+}
---
+++ src/com/leinuo/templateMethodPattern/HummerModel.java
@@ -0,0 +1,35 @@
+package com.leinuo.templateMethodPattern;
+
+/**
+ * Create by leinuo on 20-1-16 下午3:30
+ * <p>
+ * qq:1321404703 https://github.com/leinuo2016
+ */
+public abstract class HummerModel {
+
+    protected abstract void start();
+
+    protected abstract void stop();
+
+    protected abstract void alarm();
+
+    protected abstract void engineBoom();
+
+    final public void run(){
+        //先发动汽车
+        this.start();
+        //引擎开始轰鸣
+        this.engineBoom();
+        //然后就开始跑了
+        if(this.isAlarm()){
+            this.alarm();
+        }
+        //到达目的地就停车
+        this.stop();
+    };
+
+    protected boolean isAlarm() {
+        return true;
+    }
+
+}
