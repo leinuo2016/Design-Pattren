2e15fb32c132b739eb2caccd04aa4a0c0d8249e8
liujb <1321404703@qq.com>
2020-02-03 12:54:19 +0800

    原型模式

---
+++ src/com/leinuo/prototypePattern/AdvTemplate.java
@@ -0,0 +1,21 @@
+package com.leinuo.prototypePattern;
+
+/**
+ * Create by leinuo on 2020/2/3 下午12:16
+ * <p>
+ * qq:1321404703 https://github.com/leinuo2016
+ */
+public class AdvTemplate {
+
+    private String advSubject = "XX银行国庆银行卡抽奖活动";
+
+    private String advContext = "国庆抽奖活动通知：只要刷卡就送你1百万！....";
+
+    public String getAdvSubject() {
+        return this.advSubject;
+    }
+
+    public String getAdvContext() {
+        return this.advContext;
+    }
+}
---
+++ src/com/leinuo/prototypePattern/Mail.java
@@ -0,0 +1,81 @@
+package com.leinuo.prototypePattern;
+
+/**
+ * Create by leinuo on 2020/2/3 下午12:20
+ * <p>
+ * qq:1321404703 https://github.com/leinuo2016
+ */
+public class Mail implements Cloneable{
+
+
+    //收件人
+    private String receiver;
+    //邮件名称
+    private String subject;
+
+    //称谓
+    private String appellation;
+
+    //邮件内容
+    private String contxt;
+
+    //邮件的尾部，一般都是加上“XXX版权所有”等信息
+    private String tail;
+
+    public Mail(AdvTemplate advTemplate){
+        this.contxt = advTemplate.getAdvContext();
+        this.subject = advTemplate.getAdvSubject();
+    }
+
+    @Override
+    public Mail clone(){
+        Mail mail =null;
+        try {
+            mail = (Mail)super.clone();
+        } catch (CloneNotSupportedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return mail;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getAppellation() {
+        return appellation;
+    }
+
+    public void setAppellation(String appellation) {
+        this.appellation = appellation;
+    }
+
+    public String getContxt() {
+        return contxt;
+    }
+
+    public void setContxt(String contxt) {
+        this.contxt = contxt;
+    }
+
+    public String getTail() {
+        return tail;
+    }
+
+    public void setTail(String tail) {
+        this.tail = tail;
+    }
+}
