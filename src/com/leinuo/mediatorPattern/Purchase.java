caaf0b431ad8d6ebb62b5a1ca261a6bc378cba18
liujb <1321404703@qq.com>
2020-02-04 18:35:17 +0800

    中介者模式

---
+++ src/com/leinuo/mediatorPattern/Purchase.java
@@ -0,0 +1,20 @@
+package com.leinuo.mediatorPattern;
+
+/**
+ * Create by leinuo on 2020/2/4 上午11:56
+ * <p>
+ * qq:1321404703 https://github.com/leinuo2016
+ */
+public class Purchase extends AbstractColleague {
+
+    public Purchase(AbstractMediator abstractMediator) {
+        super(abstractMediator);
+    }
+
+    public void buyIBMcomputer(int number){
+        super.mediator.execute("purchase.buy", number);
+    }
+    public void refuseBuyIBM(){
+        System.out.println("不再采购IBM电脑");
+    }
+}
