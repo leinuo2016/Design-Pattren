8bc44f5848c873671e5475958ca4de0da9ee9e4b
liujb <1321404703@qq.com>
2020-02-06 12:20:19 +0800

    解释器模式

---
+++ src/com/leinuo/interpreterPattern/Client.java
@@ -0,0 +1,16 @@
+package com.leinuo.interpreterPattern;
+
+/**
+ * Create by leinuo on 2020/2/6 下午12:06
+ * <p>
+ * qq:1321404703 https://github.com/leinuo2016
+ *
+ *  解释器模式
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        int result = new Minus().interpret(new Context(new Plus().interpret(new Context(9,2)),8));
+        System.out.printf("result:" +result);
+    }
+}
---
+++ src/com/leinuo/interpreterPattern/Expression.java
@@ -0,0 +1,10 @@
+package com.leinuo.interpreterPattern;
+
+/**
+ * Create by leinuo on 2020/2/6 下午12:00
+ * <p>
+ * qq:1321404703 https://github.com/leinuo2016
+ */
+public interface Expression {
+    public int interpret(Context context);
+}
